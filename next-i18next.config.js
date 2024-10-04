f0e0e60012dfd820c5dd1faedf493422fe7ee927
Krisbo <24961323+krisboblea@users.noreply.github.com>
2024-10-04 13:33:38 +1000

    localeDetection

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -11,8 +11,7 @@ module.exports = {
     i18n: {
         locales: allLanguages,
         defaultLocale: process.env.NEXT_PUBLIC_LOCALE || 'en',
-        localeDetection: false,
-        defaultNS: 'common',
+        // localeDetection: false,
     },
     backend: {
         backendOptions: [
